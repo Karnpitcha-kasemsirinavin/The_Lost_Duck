00871eeffce8e5dc31a9bc684d3d7d579584e6ab
Karnpitcha_K <88446757+Karnpitcha-kasemsirinavin@users.noreply.github.com>
2023-01-18 02:29:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,71 +4,105 @@ from duck_game import Duck_Game
 from scenes import Scenes, Walk_Scene, Game_over_scene, Intro, End_scene
 
 pygame.init()
+
 '''=============================== Setup ================================================'''
-status = True
-level = 1
-score = 0
-heart = 10
-continue_game = True
-restart = True
-display_score = 0
-round_num = 10
-# check = True
 
-run_scene = False
-walking_scene_ready = False
-intro_ready = True
-end_scene_ready = True
-con_end_scene = False
-
-bg_x = 0
-click = False
-start_game = False
-
+'''Setup for window screen'''
 screen_width = 1200
 screen_height = 800
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-'''======================================================================================='''
+'''Window icon'''
+game_icon = pygame.image.load('Pic\\Player\\Player.png')
+pygame.display.set_icon(game_icon)
 
-def draw_text(screen,text,size,color,x,y):
-    pygame.init()
-    font = pygame.font.Font('dogicapixelbold.ttf', size)
-    text_surface = font.render(text, True, color)
-    text_rect = text_surface.get_rect()
-    text_rect.topleft = (x, y)
-    screen.blit(text_surface, text_rect)
+'''set window name as game name'''
+pygame.display.set_caption('The Lost Duck')
+
+'''For checking each level of game'''
+status = True
+
+'''level in game'''
+level = 1
+
+'''start score'''
+score = 0
+
+'''start heart '''
+heart = 10
+
+'''For checking if Game Over or not'''
+continue_game = True
+
+'''start display score, will be update with the current score and pass to Game Over or end scene'''
+display_score = 0
+
+'''Number of round to play'''
+round_num = 10
+
+'''Trigger for cut scenes in each level'''
+run_scene = False
+
+'''Trigger for walking scene btw each level'''
+walking_scene_ready = False
+
+'''Trigger for intro scene'''
+intro_ready = True
+
+'''Trigger for end scene'''
+end_scene_ready = True
+
+'''Trigger for end scene loop'''
+con_end_scene = False
+
+'''Trigger for clicking the button'''
+click = False
+
+'''Trigger for game'''
+start_game = False
 
 '''---------------------------------- Main menu ------------------------------------------'''
 
+
 def call_main_menu():
-    global click, start_game, screen, run_scene, walking_scene_ready, bg_x, round_num, continue_game, score, \
-        display_score, con_end_scene
+    global click, start_game, screen, run_scene, walking_scene_ready, round_num, continue_game, score, \
+        display_score, con_end_scene, level, heart
 
+    '''Window icon'''
+    game_icon = pygame.image.load('Pic\\Player\\Player.png')
+    pygame.display.set_icon(game_icon)
+
+    '''Cloud picture in menu to calculate and check the position'''
     bg_x_cloud1 = 0
+
+    '''Setup menu bg music'''
     menu_music = pygame.mixer.Sound('Music\\Guitar-Gentle.mp3')
-    menu_music.set_volume(0.2)
+    menu_music.set_volume(0.1)
 
-
+    '''Create sound channel 1'''
     game_bg_channel = pygame.mixer.Channel(1)
+
+    '''Create sound channel 0 for menu bg music'''
     menu_sound_channel = pygame.mixer.Channel(0)
 
-    click_sound = pygame.mixer.Sound('Sound_effect\\clicking_sound.wav')
-    click_sound.set_volume(0.3)
+    '''Play menu bg music in channel 0'''
+    game_bg_channel.play(menu_music, loops=-1)
 
-    game_bg_channel.play(menu_music)
+    '''Setup click sound effect'''
+    click_sound = pygame.mixer.Sound('Sound_effect\\clicking_sound.wav')
+    click_sound.set_volume(0.2)
 
     while True:
 
         continue_game = True
-        round_num = 1
+        round_num = 10
         clock = pygame.time.Clock()
 
         screen.fill((0, 0, 0))
 
         mx, my = pygame.mouse.get_pos()
 
-        button_1 = pygame.Rect(450, 500, 300, 100) ###left top width height
+        button_1 = pygame.Rect(450, 500, 300, 100)  # left top width height
         button_2 = pygame.Rect(450, 650, 300, 100)
 
         if button_1.collidepoint((mx, my)):
@@ -78,54 +112,54 @@ def call_main_menu():
                 menu_music.fadeout(30)
                 pygame.time.delay(300)
 
-
                 clock = pygame.time.Clock()
 
-
-                music_name = 'Music\\level\\level1_1.mp3'
-                music = pygame.mixer.Sound(music_name)
-
+                '''Play game loop for ten rounds'''
                 while round_num != 0 and continue_game is not False:
-                    '''Call intro scene for first time'''
+                    '''Call intro scene for first round'''
                     if round_num == 10:
-                        call_intro()
+                        call_intro(game_bg_channel)
 
-
-                    music_name = 'Music\\level\\level' + str(11-round_num) + '_' + str(11-round_num) + '.mp3'
-
+                    '''Generate music for each level'''
+                    music_name = 'Music\\level\\level' + str(11 - round_num) + '_' + str(11 - round_num) + '.mp3'
                     music = pygame.mixer.Sound(music_name)
-                    music.set_volume(0.2)
-
+                    music.set_volume(0.1)
                     game_bg_channel.play(music, loops=-1)
 
+                    '''call game for each round and return value to check if it is Game Over or not, and return
+                     score to display in Game Over and End scenes.'''
                     continue_game, display_score = call_duck_game(game_bg_channel, music)
 
+                    '''If not Game Over in each level, play cut scene then minus round by one'''
                     if continue_game is not False:
-                        game_bg_channel.pause()
-                        game_bg_channel.play(music)
+                        game_bg_channel.unpause()
                         call_scene()
                         call_walk_scene(music)
                         round_num -= 1
 
-                    # music.fadeout(30)
-
-                    # if pygame.mixer.music.get_busy() is True:
-                    #     music.fadeout(10)
-
+                '''Trigger end scene'''
                 if continue_game is not False:
                     con_end_scene = True
 
+                '''If not Game Over play end scene'''
                 while con_end_scene is not False:
-                    con_end_scene = call_end_scene()
+                    con_end_scene = call_end_scene(display_score)
 
+                '''If Game Over lead to end scene'''
                 while continue_game is False:
-
-                    music.stop()
+                    game_bg_channel.stop()
                     continue_game = call_game_over_scene(display_score)
 
+                '''Back to main menu continue playing menu music'''
                 menu_music.play(loops=-1)
 
                 clock.tick(60)
+
+                if round_num == 0:
+                    level = 1
+                    heart = 10
+                    score = 0
+
         if button_2.collidepoint((mx, my)):
             if click:
                 menu_sound_channel.play(click_sound)
@@ -133,7 +167,6 @@ def call_main_menu():
                 pygame.time.delay(300)
                 pygame.quit()
                 sys.exit()
-
 
         pygame.draw.rect(screen, (255, 0, 0), button_1)
         pygame.draw.rect(screen, (255, 0, 0), button_2)
@@ -158,17 +191,13 @@ def call_main_menu():
 
         cloud_1 = pygame.image.load('Pic\\bg\\bg_menu_cloud_1.png')
 
-
         rel_bg_x_cloud1 = bg_x_cloud1 % cloud_1.get_rect().width
-        screen.blit(cloud_1, (rel_bg_x_cloud1 - cloud_1.get_rect().width, 0 ))
-
+        screen.blit(cloud_1, (rel_bg_x_cloud1 - cloud_1.get_rect().width, 0))
 
         if rel_bg_x_cloud1 < screen_width:
             screen.blit(cloud_1, (rel_bg_x_cloud1, 0))
 
-
         bg_x_cloud1 -= 3
-
 
         game_name = pygame.image.load('Pic\\menu_icon.png')
         screen.blit(game_name, (300, 50))
@@ -178,12 +207,12 @@ def call_main_menu():
         screen.blit(exit_button, (450, 650))
 
         menu_text()
-
-
         pygame.display.update()
         clock.tick(60)
 
+
 '''-----------------------------------------------------------------------------------------------'''
+
 
 def menu_text():
     font = pygame.font.Font('dogicapixel.ttf', 40)
@@ -191,27 +220,30 @@ def menu_text():
     start_rect_shadow = start_text_shadow.get_rect(topright=(685, 530))
     screen.blit(start_text_shadow, start_rect_shadow)
 
-    start_text = font.render(f'START', False, (255,255,255))
-    start_rect = start_text.get_rect(topright= (690,530))
+    start_text = font.render(f'START', False, (255, 255, 255))
+    start_rect = start_text.get_rect(topright=(690, 530))
     screen.blit(start_text, start_rect)
 
     exit_text_shadow = font.render(f'EXIT', False, (33, 73, 42))
     exit_rect_shadow = exit_text_shadow.get_rect(topright=(665, 680))
     screen.blit(exit_text_shadow, exit_rect_shadow)
 
-    exit_text = font.render(f'EXIT', False, (255,255,255))
-    exit_rect = exit_text.get_rect(topright= (670,680))
+    exit_text = font.render(f'EXIT', False, (255, 255, 255))
+    exit_rect = exit_text.get_rect(topright=(670, 680))
     screen.blit(exit_text, exit_rect)
 
+
 '''====================================== Duck game =============================================='''
+
+
 def call_duck_game(game_bg_channel, music):
-    global bg_x, level, screen, status, score, heart, run_scene, walking_scene_ready, screen_width, screen_height, \
+    global level, screen, status, score, heart, run_scene, walking_scene_ready, screen_width, screen_height, \
         continue_game, display_score
 
-    ENEMIESLASER = pygame.USEREVENT + 1  ###
+    ENEMIESLASER = pygame.USEREVENT + 1
     pygame.time.set_timer(ENEMIESLASER, 800)
 
-    pygame.time.delay(300)
+    pygame.time.delay(30)
 
     status = True
     game = Duck_Game(level, score, heart, status, game_bg_channel, music)
@@ -223,67 +255,53 @@ def call_duck_game(game_bg_channel, music):
             if event.type == ENEMIESLASER:
                 game.enemies_shoot()
 
-
-        screen.fill(((0, 0, 0)))
+        screen.fill((0, 0, 0))
         background_image = 'Pic\\bg\\bg' + str(game.level) + '.png'
         background = pygame.image.load(background_image)
         screen.blit(background, (0, 0))
-
 
         '''Method in class Duck_Game'''
         continue_game = game.running_game()
         status = game.check_status()
 
         pygame.display.flip()
-        # print(continue_game)
-
 
     '''updating score and heart'''
 
-    if continue_game != False:
+    if continue_game is not False:
         level += 1
         score = game.score
         heart = game.heart
+        display_score = game.score
 
     else:
         heart = 10
         display_score = game.score
         score = 0
-
-        '''If want to make a replay don't set level'''
-
         level = 1
 
-    return continue_game,display_score
-
-
-
+    return continue_game, display_score
 
 
 def call_scene():
-
     run_scene = True
+
     '''Scene loop'''
     scene = Scenes(level, screen)
-    while run_scene == True or run_scene == None:
+    while run_scene is True or run_scene is None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        screen.fill(((0, 0, 0)))
-        # background_image = 'Pic\\bg\\bg' + str(game.level) + '.png'
-
-
-        scene.runnning_scene()
+        scene.running_scene()
         run_scene = scene.check_con()
 
         pygame.display.flip()
 
+
 def call_walk_scene(music):
-
     walking_scene_ready = True
-
     '''Walking scene'''
     walk_scene = Walk_Scene(level, screen, music)
     while walking_scene_ready is True or walking_scene_ready is None:
@@ -295,15 +313,18 @@ def call_walk_scene(music):
         walk_scene.running_scene()
         walking_scene_ready = walk_scene.check_con()
 
-
-
         pygame.display.flip()
 
-def call_game_over_scene(score):
 
+def call_game_over_scene(score):
     global screen
     game_over_scene_ready = True
 
+    '''Game Over sound'''
+    game_over_sound = pygame.mixer.Sound('Sound_effect\\game_over.wav')
+    game_over_sound.set_volume(0.1)
+
+    game_over_sound.play()
 
     '''Game over scene'''
 
@@ -317,28 +338,19 @@ def call_game_over_scene(score):
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
+                    game_over_sound.stop()
                     return True
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-
-
 
         game_over_scene_ready = game_over_scene.update()
 
-
-        # if game_over_scene_ready == False:
-        #     return True
-
         pygame.display.flip()
 
-def call_intro():
 
+def call_intro(game_bg_channel):
     intro_ready = True
 
-
     '''Intro scene'''
-    intro_scene = Intro(screen)
+    intro_scene = Intro(screen, game_bg_channel)
     while intro_ready is True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -350,12 +362,11 @@ def call_intro():
         pygame.display.flip()
 
 
-def call_end_scene():
-
+def call_end_scene(display_score):
     end_scene_ready = True
 
     '''End scene'''
-    end_scene = End_scene(screen)
+    end_scene = End_scene(screen, display_score)
     while end_scene_ready is True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -367,6 +378,7 @@ def call_end_scene():
         pygame.display.flip()
 
     return end_scene_ready
+
 
 ##################################################################################################################
 if __name__ == '__main__':
